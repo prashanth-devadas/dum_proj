2693e435b5d9123cbcda9adf6ecb13f8de9e5962
prashanth-devadas <prashanth.devadas@yahoo.com>
2020-06-10 02:33:57 -0400

    added a simple model and couple of routers with GET and POST methods

---
+++ routes/subscribers.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const Subscriber = require('../models/subscriber');
+
+// Get all subscribers
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find();
+        res.json(subscribers);
+    } catch (err) {
+        res.status(500).json({ message: err.message});
+    }
+});
+
+// Get one subscriber
+router.get('/:id', (req, res) => {
+
+});
+
+// Create one subscriber
+router.post('/', async (req, res)=> {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedChannel: req.body.subscribedChannel
+    })
+
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({ message: err.message});
+    }
+});
+
+// Update one subscriber
+router.patch('/:id', (req, res) => {
+
+});
+
+// Delete one subscriber
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('connected to database'));
+
+app.use(express.json());
+
+const subscriberRouter = require('./routes/subscribers');
+app.use('/subscribers', subscriberRouter);
 
 app.listen(3000, () => console.log('listening on port 3000'));
